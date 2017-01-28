be919f9be133447ba8079e40e5f904af580836c3
ellenaua <elenasharovar@gmail.com>
2017-01-28 08:03:24 +0200

    Serve files using express.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+// serve your elements
+app.use('/src', express.static('src'));
+// serve bower_components
+app.use('/bower_components', express.static('bower_components'));
+// serve index.html
+app.get('/', function(req, res){
+  res.sendfile('views/index.html');
+});
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log(`App listening on port ${port}!`);
+});
